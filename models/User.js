61b18e72ae512c41deaf1d55f9b222877776fc8e
tfavro <wetr9902@gmail.com>
2022-03-25 10:47:36 -0500

    server and user module

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+    
+});
+
+//retrieves the length of the user's friends array field on query.
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+//create the user model using the userSchema
+const User = model('User', UserSchema);
+
+//export the User model
+module.exports = User;
